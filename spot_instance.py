3635a5e012066dc9c823e51bc48199d04e5156b9
Stephen Cott <stephen.cott@expeditors.com>
2018-08-06 13:47:20 -0700

    Added argparse for parsing arguments

--- spot_instance.py
+++ spot_instance.py
@@ -1,13 +1,38 @@
 import boto3
 import sys
+import argparse
 
+AMI = 'ami-f2d3638a'
+INSTANCE_TYPE = 't2.micro'
 REGION = 'us-west-2'
+KEYNAME = 'cotste-us-west-2'
+
+parser = argparse.ArgumentParser()
+parser.add_argument( '-r', '--region', nargs='?', default=REGION, const=REGION)
+parser.add_argument( '-a', '--ami', nargs='?', default=AMI, const=AMI)
+parser.add_argument( '-i', '--instance_type', nargs='?', default=INSTANCE_TYPE, const=INSTANCE_TYPE)
+parser.add_argument( '-k', '--keyname', nargs='?', default=KEYNAME, const=KEYNAME)
+
+parseargs = vars(parser.parse_args())
+
+region = parseargs.get('region')
+instance_type = parseargs.get('instance_type')
+ami = parseargs.get('ami')
+keyname = parseargs.get('keyname')
+
+for a in parseargs:
+    print(parseargs.get(a))
+
+if parseargs.get('region'):
+    print( 'Region: ', parseargs.get('region'))
+    
+
 #del_instance = sys.argv[1]
 
-def delete_spot() :
+def delete_aspot():
 
     #ec2 = boto3.resource( 'ec2', region_name=REGION)
-    client = boto3.client( 'ec2', region_name=REGION)
+    client = boto3.client( 'ec2', region_name=region)
 
     print( 'Getting active spot instance requests...')
 
@@ -68,4 +93,37 @@ def delete_spot() :
     else :
         print('\nNo active spot requests found')
         exit(0)
+
+def create_spot( instance_type, ami, region) :
+    
+    import boto3
+    import time
+
+    if argv.length < 2 :
+        instance_type = INSTANCE_TYPE
+        region = REGION
+        
+    ec2 = boto3.resource('ec2', region_name=region)
+
+    instance = ec2.create_instances(
+            ImageId = AMI,
+            InstanceType = INSTANCE_TYPE,
+            KeyName= KEYNAME,
+            MinCount = 1,
+            MaxCount = 1,
+            InstanceMarketOptions = {
+                'MarketType' : 'spot',
+                'SpotOptions' : {
+                    'SpotInstanceType' : 'one-time',
+                    'InstanceInterruptionBehavior' : 'terminate'
+                }
+            }
+
+            )
+
+    instance.wait_until_running()
+
+    print( instance['InstanceId']) 
+
+#def map_rout53() :
 
